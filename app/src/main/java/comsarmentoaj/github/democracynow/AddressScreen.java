13dc0710f5cc4acb819d6bda146bccf034151470
Austin Sarmento <sarmentoaj@vcu.edu>
2017-06-20 19:06:11 -0400

    Lets start api worl

--- app/src/main/java/comsarmentoaj/github/democracynow/AddressScreen.java
+++ app/src/main/java/comsarmentoaj/github/democracynow/AddressScreen.java
@@ -5,9 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AddressScreen extends AppCompatActivity {
 
+    String uri = "https://www.googleapis.com/civicinfo/v2";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_screen);
+
+
+    }
+
+
+    public void freedom() {
     }
 }
