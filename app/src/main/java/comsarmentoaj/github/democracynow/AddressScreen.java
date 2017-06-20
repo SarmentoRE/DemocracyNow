dfef263f40e0da792bfb5cde3872f7f631c41b2d
Austin Sarmento <sarmentoaj@vcu.edu>
2017-06-20 14:44:16 -0400

    Initial commit

---
+++ app/src/main/java/comsarmentoaj/github/democracynow/AddressScreen.java
@@ -0,0 +1,13 @@
+package comsarmentoaj.github.democracynow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddressScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_screen);
+    }
+}
